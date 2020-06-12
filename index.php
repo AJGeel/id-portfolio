1cdcd5eb4a501c13959d09ac83180ba68fcebd98
AJGeel <geelaj01@gmail.com>
2020-06-12 12:13:04 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+      Reflective Portfolio by Arthur Geel
+      Proudly developed from scratch using HTML/CSS/JS/PHP.
+
+      Interested in the source code? 
+-->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
+    <title>Portfolio — Arthur Geel</title>
+    <meta name="description" content="Welcome to the reflective portfolio of Arthur Geel. On this page, I document and reflect on the development in the expertise areas developed in my Master's study Industrial Design at the TU/e.">
+    <meta name="keywords" content="Arthur Geel">
+    <meta name="author" content="Arthur Geel, hello@arthurgeel.com">
+
+    <link rel="shortcut icon" href="/i/logo.png" type="image/png" id="favicon">
+    <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/normalize.min.css"> <!-- normalize.css v8.0.1. MIT License -->
+    <link rel="stylesheet" href="/css/style.min.css">
+  </head>
+
+  <body>
+
+    <!-- Sticky navigational menu, -->
+    <nav>
+
+    </nav>
+
+  </body>
+
+  <script src="js/main.min.js" charset="utf-8"></script>
+</html>
